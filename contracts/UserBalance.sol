806b5f0dfa476c4987a4e9eeb820ae07067fa697
Krzysztof Spisak-Spisacki <mafiagustawa@gmail.com>
2024-08-07 19:09:50 +0200

    ERC20 balance

--- contracts/UserBalance.sol
+++ contracts/UserBalance.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract UserBalance {
-    mapping(address => bool) public whitelist;
     mapping(address => uint256) private ethBalances;
+    mapping(address => mapping (address => uint256)) private erc20balances;
+    mapping(address => bool) public whitelist;
 
     constructor(address whitelisted){
         whitelist[whitelisted] = true;
@@ -19,14 +20,28 @@ contract UserBalance {
         return ethBalances[user];
     }
 
+    function changeERC20balance(address erc20coin, address userToChange, int256 amount) external onlyWhitelisted {
+        require(-amount <= int256(erc20balances[erc20coin][userToChange]), "You are trying to reduce balance by higher amount than it is");
+
+        _changeERC20balance(erc20coin, userToChange, amount);
+    }
+
+    function getERC20balance(address erc20coin, address user) external view onlyWhitelisted returns(uint256)  {
+        return erc20balances[erc20coin][user];
+    }
+
     function addToWhitelist(address newWhitelisted) external onlyWhitelisted {
         require(!whitelist[newWhitelisted], "This address is already whitelisted");
 
         _addToWhitelist(newWhitelisted);
     }
 
-    function _changeEthBalance(address userToChange, int256 amount) private onlyWhitelisted {
+    function _changeEthBalance(address userToChange, int256 amount) private {
         ethBalances[userToChange] = uint256(int256(ethBalances[userToChange]) + amount);
+    }
+
+    function _changeERC20balance(address erc20coin, address userToChange, int256 amount) private {
+        erc20balances[erc20coin][userToChange] = uint256(int256(erc20balances[erc20coin][userToChange]) + amount);
     }
 
     function _addToWhitelist(address newWhitelisted) private {
--- test/UserBalance.ts
+++ test/UserBalance.ts
@@ -7,6 +7,9 @@ import hre from "hardhat";
 describe("UserBalance.sol", function () {
 
   const SOME_ADDRESS = '0x13CB6AE34A13a0977F4d7101eBc24B87Bb23F0d5';
+  const SOME_OTHER_ADDRESS = '0x49625758cD76c5AbBF8754327aCC658825fc46E7';
+  const USDC_SEPOLIA_ADDRESS = '0xCD6b2a3DE83182af377AEC5d71C261A0031E2fB8';
+  const USDC_DAI_ADDRESS = '0xB5f214A5a9a12705A2Bc0cA94ddad7f9f425c46b';
   const SOME_POSITIVE_AMOUNT = 10;
   const SOME_SMALLER_NEGATIVE_AMOUNT = -5;
 
@@ -61,6 +64,10 @@ describe("UserBalance.sol", function () {
       expect(await sut.connect(initAccount).getEthBalance(SOME_ADDRESS)).to.equal(SOME_POSITIVE_AMOUNT);
       expect(await sut.connect(initAccount).changeEthBalance(SOME_ADDRESS, SOME_SMALLER_NEGATIVE_AMOUNT)).not.to.be.reverted;
       expect(await sut.connect(initAccount).getEthBalance(SOME_ADDRESS)).to.equal(SOME_POSITIVE_AMOUNT + SOME_SMALLER_NEGATIVE_AMOUNT);
+
+      expect(await sut.connect(initAccount).getEthBalance(SOME_OTHER_ADDRESS)).to.equal(0);
+      expect(await sut.connect(initAccount).changeEthBalance(SOME_OTHER_ADDRESS, SOME_POSITIVE_AMOUNT)).not.to.be.reverted;
+      expect(await sut.connect(initAccount).getEthBalance(SOME_OTHER_ADDRESS)).to.equal(SOME_POSITIVE_AMOUNT);
     });
 
     it("Should not decrease ETH balance by more than its value", async function () {
@@ -68,6 +75,44 @@ describe("UserBalance.sol", function () {
 
       expect(await sut.connect(initAccount).changeEthBalance(SOME_ADDRESS, SOME_POSITIVE_AMOUNT)).not.to.be.reverted;
       await expect(sut.connect(initAccount).changeEthBalance(SOME_ADDRESS, -(SOME_POSITIVE_AMOUNT + 1))).to.be.revertedWith(
+          'You are trying to reduce balance by higher amount than it is'
+      );
+    });
+  });
+
+  describe("ERC20balance", function () {
+    it("Should not allow unauthorised address to change ERC20 balance", async function () {
+      const {sut, ownerAccount} = await loadFixture(deployFixture);
+
+      await expect(sut.connect(ownerAccount).changeERC20balance(USDC_SEPOLIA_ADDRESS, SOME_ADDRESS, SOME_POSITIVE_AMOUNT)).to.be.revertedWith(
+          'Caller address is not whitelisted'
+      );
+    });
+
+    it("Should change ERC20 balance", async function () {
+      const {sut, initAccount} = await loadFixture(deployFixture);
+
+      expect(await sut.connect(initAccount).getERC20balance(USDC_SEPOLIA_ADDRESS, SOME_ADDRESS)).to.equal(0);
+      expect(await sut.connect(initAccount).changeERC20balance(USDC_SEPOLIA_ADDRESS, SOME_ADDRESS, SOME_POSITIVE_AMOUNT)).not.to.be.reverted;
+      expect(await sut.connect(initAccount).getERC20balance(USDC_SEPOLIA_ADDRESS, SOME_ADDRESS)).to.equal(SOME_POSITIVE_AMOUNT);
+      expect(await sut.connect(initAccount).changeERC20balance(USDC_SEPOLIA_ADDRESS, SOME_ADDRESS, SOME_SMALLER_NEGATIVE_AMOUNT)).not.to.be.reverted;
+      expect(await sut.connect(initAccount).getERC20balance(USDC_SEPOLIA_ADDRESS, SOME_ADDRESS)).to.equal(SOME_POSITIVE_AMOUNT + SOME_SMALLER_NEGATIVE_AMOUNT);
+
+
+      expect(await sut.connect(initAccount).getERC20balance(USDC_SEPOLIA_ADDRESS, SOME_OTHER_ADDRESS)).to.equal(0);
+      expect(await sut.connect(initAccount).changeERC20balance(USDC_SEPOLIA_ADDRESS, SOME_OTHER_ADDRESS, SOME_POSITIVE_AMOUNT)).not.to.be.reverted;
+      expect(await sut.connect(initAccount).getERC20balance(USDC_SEPOLIA_ADDRESS, SOME_OTHER_ADDRESS)).to.equal(SOME_POSITIVE_AMOUNT);
+
+      expect(await sut.connect(initAccount).getERC20balance(USDC_DAI_ADDRESS, SOME_ADDRESS)).to.equal(0);
+      expect(await sut.connect(initAccount).changeERC20balance(USDC_DAI_ADDRESS, SOME_ADDRESS, SOME_POSITIVE_AMOUNT)).not.to.be.reverted;
+      expect(await sut.connect(initAccount).getERC20balance(USDC_DAI_ADDRESS, SOME_ADDRESS)).to.equal(SOME_POSITIVE_AMOUNT);
+    });
+
+    it("Should not decrease ERC20 balance by more than its value", async function () {
+      const {sut, initAccount} = await loadFixture(deployFixture);
+
+      expect(await sut.connect(initAccount).changeERC20balance(USDC_SEPOLIA_ADDRESS, SOME_ADDRESS, SOME_POSITIVE_AMOUNT)).not.to.be.reverted;
+      await expect(sut.connect(initAccount).changeERC20balance(USDC_SEPOLIA_ADDRESS, SOME_ADDRESS, -(SOME_POSITIVE_AMOUNT + 1))).to.be.revertedWith(
           'You are trying to reduce balance by higher amount than it is'
       );
     });
