a235b3f39846a1201e7183b7f86815e001d8c2f4
Krzysztof Spisak-Spisacki <mafiagustawa@gmail.com>
2024-08-07 18:34:46 +0200

    ETH balance

--- contracts/UserBalance.sol
+++ contracts/UserBalance.sol
@@ -3,15 +3,30 @@ pragma solidity ^0.8.0;
 
 contract UserBalance {
     mapping(address => bool) public whitelist;
+    mapping(address => uint256) private ethBalances;
 
     constructor(address whitelisted){
         whitelist[whitelisted] = true;
+    }
+
+    function changeEthBalance(address userToChange, int256 amount) external onlyWhitelisted {
+        require(-amount <= int256(ethBalances[userToChange]), "You are trying to reduce balance by higher amount than it is");
+
+        _changeEthBalance(userToChange, amount);
+    }
+
+    function getEthBalance(address user) external view onlyWhitelisted returns(uint256)  {
+        return ethBalances[user];
     }
 
     function addToWhitelist(address newWhitelisted) external onlyWhitelisted {
         require(!whitelist[newWhitelisted], "This address is already whitelisted");
 
         _addToWhitelist(newWhitelisted);
+    }
+
+    function _changeEthBalance(address userToChange, int256 amount) private onlyWhitelisted {
+        ethBalances[userToChange] = uint256(int256(ethBalances[userToChange]) + amount);
     }
 
     function _addToWhitelist(address newWhitelisted) private {
--- test/UserBalance.ts
+++ test/UserBalance.ts
@@ -7,41 +7,69 @@ import hre from "hardhat";
 describe("UserBalance.sol", function () {
 
   const SOME_ADDRESS = '0x13CB6AE34A13a0977F4d7101eBc24B87Bb23F0d5';
+  const SOME_POSITIVE_AMOUNT = 10;
+  const SOME_SMALLER_NEGATIVE_AMOUNT = -5;
 
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
   async function deployFixture() {
-    // Contracts are deployed using the first signer/account by default
     const [ownerAccount, initAccount] = await hre.ethers.getSigners();
 
     const UserBalanceFactory = await hre.ethers.getContractFactory("UserBalance");
-    console.log(`address init: ${initAccount.address}`);
     const sut = await UserBalanceFactory.deploy(initAccount.address);
     await sut.waitForDeployment();
 
     return { sut, ownerAccount,  initAccount };
   }
 
-  it("Should allow initial address to modify whitelist", async function () {
-    const { sut, initAccount } = await loadFixture(deployFixture);
+  describe("Whitelist", function () {
+    it("Should allow initial address to modify whitelist", async function () {
+      const {sut, initAccount} = await loadFixture(deployFixture);
 
-    expect(await sut.connect(initAccount).addToWhitelist(SOME_ADDRESS)).not.to.be.reverted;
+      expect(await sut.connect(initAccount).addToWhitelist(SOME_ADDRESS)).not.to.be.reverted;
+    });
+
+    it("Should not allow unauthorised addresses to modify whitelist", async function () {
+      const {sut, ownerAccount} = await loadFixture(deployFixture);
+
+      await expect(sut.connect(ownerAccount).addToWhitelist(SOME_ADDRESS)).to.be.revertedWith(
+          'Caller address is not whitelisted'
+      );
+    });
+
+    it("Should not whitelisting the same address twice", async function () {
+      const {sut, initAccount} = await loadFixture(deployFixture);
+
+      await expect(sut.connect(initAccount).addToWhitelist(initAccount.address)).to.be.revertedWith(
+          'This address is already whitelisted'
+      );
+    });
   });
 
-  it("Should not allow unauthorised addresses to modify whitelist", async function () {
-    const { sut, ownerAccount } = await loadFixture(deployFixture);
+  describe("ETHbalance", function () {
+    it("Should not allow unauthorised address to change ETH balance", async function () {
+      const {sut, ownerAccount} = await loadFixture(deployFixture);
 
-    await expect(sut.connect(ownerAccount).addToWhitelist(SOME_ADDRESS)).to.be.revertedWith(
-        'Caller address is not whitelisted'
-    );
-  });
+      await expect(sut.connect(ownerAccount).changeEthBalance(SOME_ADDRESS, SOME_POSITIVE_AMOUNT)).to.be.revertedWith(
+          'Caller address is not whitelisted'
+      );
+    });
 
-  it("Should not whitelisting the same address twice", async function () {
-    const { sut, initAccount } = await loadFixture(deployFixture);
+    it("Should change ETH balance", async function () {
+      const {sut, initAccount} = await loadFixture(deployFixture);
 
-    await expect(sut.connect(initAccount).addToWhitelist(initAccount.address)).to.be.revertedWith(
-        'This address is already whitelisted'
-    );
+      expect(await sut.connect(initAccount).getEthBalance(SOME_ADDRESS)).to.equal(0);
+      expect(await sut.connect(initAccount).changeEthBalance(SOME_ADDRESS, SOME_POSITIVE_AMOUNT)).not.to.be.reverted;
+      expect(await sut.connect(initAccount).getEthBalance(SOME_ADDRESS)).to.equal(SOME_POSITIVE_AMOUNT);
+      expect(await sut.connect(initAccount).changeEthBalance(SOME_ADDRESS, SOME_SMALLER_NEGATIVE_AMOUNT)).not.to.be.reverted;
+      expect(await sut.connect(initAccount).getEthBalance(SOME_ADDRESS)).to.equal(SOME_POSITIVE_AMOUNT + SOME_SMALLER_NEGATIVE_AMOUNT);
+    });
+
+    it("Should not decrease ETH balance by more than its value", async function () {
+      const {sut, initAccount} = await loadFixture(deployFixture);
+
+      expect(await sut.connect(initAccount).changeEthBalance(SOME_ADDRESS, SOME_POSITIVE_AMOUNT)).not.to.be.reverted;
+      await expect(sut.connect(initAccount).changeEthBalance(SOME_ADDRESS, -(SOME_POSITIVE_AMOUNT + 1))).to.be.revertedWith(
+          'You are trying to reduce balance by higher amount than it is'
+      );
+    });
   });
 });
